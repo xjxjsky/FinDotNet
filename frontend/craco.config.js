8ae5eebbd3940c22b5c9fd0ed1a6cbb285df0692
js <xjxjsky@gmail.com>
2024-09-30 23:43:07 +1000

    Replace react-scripts with craco and configure Babel and Jest

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,10 +1,12 @@
 // craco.config.js
 module.exports = {
     babel: {
-      plugins: [
-        "@babel/plugin-proposal-private-property-in-object"
-      ],
-    },
+        plugins: [
+          ["@babel/plugin-transform-class-properties", { "loose": true }],
+          ["@babel/plugin-transform-private-methods", { "loose": true }],
+          ["@babel/plugin-proposal-private-property-in-object", { "loose": true }]
+        ]
+      },
     jest: {
       configure: {
         transformIgnorePatterns: [
